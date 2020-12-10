905a8eb73965dbcebcc1cf586c9ba357137413aa
TrogChamp <71532570+TrogChamp@users.noreply.github.com>
2020-12-10 17:41:14 -0500

    Add files via upload
    Dark Mode

--- final.py
+++ final.py
@@ -1,60 +1,66 @@
 import tkinter
-from tkinter import *
+import tkinter.ttk as ttk
+from ttkthemes import ThemedStyle
+
 import webbrowser
 
 url = 'https://tarkov-market.com/'
 
 window = tkinter.Tk()  # Create the window
+window['background'] = '#414141'
 window.title("Escape From Tarkov Profit Calculator") # Change The Window Name
 
-tkinter.Label(window, text="Enter Unique Item Material Cost").grid(row=0) # First Line to enter
+style = ThemedStyle(window)
+style.set_theme("equilux")
+
+ttk.Label(window, text="Enter Unique Item Material Cost").grid(row=0) # First Line to enter
 
 # Creating text prompt and entry window for the first item
-e1 = tkinter.Entry(window)
-tkinter.Label(window, text="First Unique Item").grid(row=1)
+e1 = ttk.Entry(window)
+ttk.Label(window, text="First Unique Item").grid(row=1)
 e1.grid(row=1, column=1)
 
 #Create first quanity box
-a1 = tkinter.Entry(window)
-tkinter.Label(window, text="Amount").grid(row=1,column=2)
+a1 = ttk.Entry(window)
+ttk.Label(window, text="Amount").grid(row=1,column=2)
 a1.grid(row=1, column=3)
 
 
 # Creating text prompt and entry window for the second item
-e2 = tkinter.Entry(window)
-tkinter.Label(window, text="Second Unique Item").grid(row=2)
+e2 = ttk.Entry(window)
+ttk.Label(window, text="Second Unique Item").grid(row=2)
 e2.grid(row=2, column=1)
 
-a2 = tkinter.Entry(window)
-tkinter.Label(window, text="Amount").grid(row=2,column=2)
+a2 = ttk.Entry(window)
+ttk.Label(window, text="Amount").grid(row=2,column=2)
 a2.grid(row=2, column=3)
 
 # Creating text prompt and entry window for the third item
-e3 = tkinter.Entry(window)
-tkinter.Label(window, text="Third Unique Item").grid(row=3)
+e3 = ttk.Entry(window)
+ttk.Label(window, text="Third Unique Item").grid(row=3)
 e3.grid(row=3, column=1)
 
-a3 = tkinter.Entry(window)
-tkinter.Label(window, text="Amount").grid(row=3,column=2)
+a3 = ttk.Entry(window)
+ttk.Label(window, text="Amount").grid(row=3,column=2)
 a3.grid(row=3, column=3)
 
 # Creating text prompt and entry window for the fourth item
-e4 = tkinter.Entry(window)
-tkinter.Label(window, text="Fourth Unique Item").grid(row=4)
+e4 = ttk.Entry(window)
+ttk.Label(window, text="Fourth Unique Item").grid(row=4)
 e4.grid(row=4, column=1)
 
-a4 = tkinter.Entry(window)
-tkinter.Label(window, text="Amount").grid(row=4,column=2)
+a4 = ttk.Entry(window)
+ttk.Label(window, text="Amount").grid(row=4,column=2)
 a4.grid(row=4, column=3)
 
 #Creating Text Prompt and entry window for the sale price of the item produced from the craft
-product = tkinter.Entry(window)
-tkinter.Label(window, text="Enter The Price of the Product from The Craft").grid(row=6)
+product = ttk.Entry(window)
+ttk.Label(window, text="Enter The Price of the Product from The Craft").grid(row=6)
 product.grid(row=6, column=1)
 
 #Creating Text Prompt and entry window for the time it takes to complete the craft
-minutes = tkinter.Entry(window)
-tkinter.Label(window, text="Enter the Time in Minutes it Takes to Complete The Craft").grid(row=7)
+minutes = ttk.Entry(window)
+ttk.Label(window, text="Enter the Time in Minutes it Takes to Complete The Craft").grid(row=7)
 minutes.grid(row=7, column=1)
 
 #Method to calculate profit
@@ -71,9 +77,10 @@ def getProfit():
     tkinter.Label(window,text=epic2).grid(row=10)
 
 #Button that calls the profit method
-button = tkinter.Button(text="Calculate Profit", command=getProfit)
+button = ttk.Button(text="Calculate Profit", command=getProfit)
 button.grid(row=8, column=1)
 
-button = tkinter.Button(text="Live Market", command = lambda : webbrowser.open_new_tab(url))
+button = ttk.Button(text="Live Market", command = lambda : webbrowser.open_new_tab(url))
 button.grid(row=11,column=1)
-mainloop()
+window.mainloop()
+
